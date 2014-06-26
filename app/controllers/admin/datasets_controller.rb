a519a15e0ec1bdc16609b0bc4547ac8c20f4d7e8
Nick Hong <hong.nick@gmail.com>
2014-06-26 01:58:10 -0700

    Add the rest of the admin dataset controller (wow, wtf)

--- app/controllers/admin/datasets_controller.rb
+++ app/controllers/admin/datasets_controller.rb
@@ -7,8 +7,31 @@ class Admin::DatasetsController < ApplicationController
     @dataset = Dataset.new
   end
 
+  def create
+    @dataset = Dataset.new(dataset_params)
+    if @dataset.save
+      flash[:notice] = "Dataset '#{@dataset.name} was created successfully."
+      redirect_to admin_dataset_path(@dataset)
+    else
+      flash[:errors] = @dataset.errors
+      render :new
+    end
+  end
+
   def edit
     @dataset = Dataset.find(params[:id])
+  end
+
+  def update
+    @dataset = Dataset.find(params[:id])
+    @dataset.assign_attributes(dataset_params)
+    if @dataset.save
+      flash[:notice] = "Dataset '#{@dataset.name} was updated successfully."
+      redirect_to admin_dataset_path(@dataset)
+    else
+      flash[:errors] = @dataset.errors
+      render :edit
+    end
   end
 
   def show
@@ -23,5 +46,11 @@ class Admin::DatasetsController < ApplicationController
       flash[:errors] = @dataset.errors
     end
     redirect_to :admin_datasets
+  end
+
+  private
+
+  def dataset_params
+    params.require(:dataset).permit(:name, :notes, :source_description, :source_url, :visualization_iframe)
   end
 end
