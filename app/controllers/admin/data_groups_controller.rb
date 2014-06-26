6ec9307f8c07af1cb0028ef3c0e83ac866253ff8
Nick Hong <hong.nick@gmail.com>
2014-06-25 20:58:34 -0700

    Add ability to create child level data group, fix seeds to allow 2 extra admins

--- app/controllers/admin/data_groups_controller.rb
+++ app/controllers/admin/data_groups_controller.rb
@@ -11,5 +11,27 @@ class Admin::DataGroupsController < ApplicationController
   end
 
   def create
+    if !data_group_params[:child_ids].nil?
+      # for super groups
+    else
+      # for non-super groups
+      data_group = DataGroup.new(data_group_params)
+      data_group.datasets = Dataset.find(params[:data_group][:dataset_ids])
+      if data_group.save
+        redirect_to admin_data_group_path data_group
+      else
+        flash[:errors] = data_group.errors
+        render :new
+      end
+    end
+  end
+
+  def show
+    @data_group = DataGroup.includes(:datasets).find(params[:id])
+    @children = DataGroup.where(parent_id: @data_group.id)
+  end
+
+  def data_group_params
+    params.require(:data_group).permit(:name, :parent_id)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Admin.create({ email: 'hong.nick@gmail.com', password: '12345678' }) if Admin.where(email: 'hong.nick@gmail.com').first.nil?
-Admin.create({ email: 'jbonaguro@gmail.com', password: '12345678' }) if Admin.where(email: 'hong.nick@gmail.com').first.nil?
-Admin.create({ email: 'niazsouti@gmail.com', password: '12345678' }) if Admin.where(email: 'hong.nick@gmail.com').first.nil?
+Admin.create({ email: 'jbonaguro@gmail.com', password: '12345678' }) if Admin.where(email: 'jbonaguro@gmail.com').first.nil?
+Admin.create({ email: 'niazsouti@gmail.com', password: '12345678' }) if Admin.where(email: 'niazsouti@gmail.com').first.nil?
 
 ['Policy #1', 'Policy #2', 'Policy #3', 'Policy #4'].each do |policy_name|
   Policy.create({
